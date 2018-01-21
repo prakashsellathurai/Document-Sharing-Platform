39377685ea69df0887cbe958dc4dc40ac0ff0509
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-01-22 01:40:42 +0530

    packaged version

--- server/api/routes/org/index.js
+++ server/api/routes/org/index.js
@@ -1,9 +1,25 @@
-// '/org/ route
+'use-strict'
 var routerLib = require('../../libraries/library')
 var router = routerLib.router
-router.route('/org')
+
+var create = require('./create')
+var read = require('./read')
+/* GET users listing. */
+
+router.route('/Org')
+.get(function (req, res, next) {
+  res.json({'msg': 'Org'})
+})
+
+router.route('/Org/:Id')
    .get(function (req, res, next) {
        // '/user/' route
-     next()
+     res.json({from: req.params})
    })
+   .post(function (req, res, next) {
+     res.json({from: req.params})
+   })
+router.route('/Org/:Id/create', create)
+router.route('/Org/:Id/read', read)
+
 module.exports = router
